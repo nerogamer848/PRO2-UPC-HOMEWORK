79b241c1bd7adcabe374f595e30cb428a0dee656
nerogamer848 <132412649+nerogamer848@users.noreply.github.com>
2023-11-07 19:57:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion{
+    class Program {
+        static void Main(string[] args ){
+
+            // este es un programa de hola mundo
+            /* este un comentario
+            * de bloque*/
+            {
+                Console.WriteLine("Hola mundo");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
