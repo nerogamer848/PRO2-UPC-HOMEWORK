9eb8675e8cdbfdcfb338ce12cb0e7fd720f82fab
nerogamer848 <132412649+nerogamer848@users.noreply.github.com>
2023-11-07 21:57:03 -0400

    Add files via upload

---
+++ codigo facilito video 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System. Threading.Tasks;
+
+namespace Codigo Facilito{
+´class program{
+static void Main(string[] args){
+    int i = 10;
+    decimal x = 12.2;m
+boll bandera = true;
+string cadena = "12344";
+Dateline fecha = DateTime.MinValue;
+i = convert.ToInt32(cadena);
+console.WriteLine("El valor de i es: {0}", i);
+console.WriteLine("El valor de x es ; (0:C)" , x);
+console.WriteLine("El valor de bandera es: "+ bandera.ToString());
+console.Write("El valor decadena: " + cadena);
+console.WriteLine("El valor de fecha : " + fecha.ToShortDateString());
+console.ReadKey();
+}
+}
+class program
+    }
