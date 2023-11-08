54727d1f37c497e284fa1cd9c354cc7bdbb92066
nerogamer848 <132412649+nerogamer848@users.noreply.github.com>
2023-11-07 20:38:22 -0400

    Add files via upload

---
+++ codigo facilito video 3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System. Threading.Tasks;
+
+namespace Codigo Facilito
+{
+    class Program{
+        static void Main(string[] args){
+            int i =0;
+            decimal x = 0.0m;
+            float f = 0.0f;
+            double d =0.0D;
+            string cadena = "hello world";
+            bool bandera =false;
+            DateTime fecha = Dateline.MinValue;
+            console.Writeline("El valor de i es: {0}", i);
+            console.WriteLine("El valor de x es : {0:C}",x);
+            console.WriteLine("El valor de f es : {0:F2}",f)
+            console.WriteLine("el valor de d es : {0:F2}", d)
+            console.WriteLine("El valor de cadena es : "+ cadena);
+            console.WriteLine("El valor de bandera es : " + bandera.ToString());
+            console.WriteLine("El valor de fecha es : " + fecha.ToShortDateString());
+            console.ReadKey();
+        }
+    }
+}
